07f6b6237dcc08aecb75f27bf7487082874277d1
Ben225 <135730564+Ben0225@users.noreply.github.com>
2023-11-21 16:57:36 +0100

    Update lance_missiles_anti_surface.py

--- weapon/lance_missiles_anti_surface.py
+++ weapon/lance_missiles_anti_surface.py
@@ -5,3 +5,9 @@ class Lance_missiles_anti_surface(Weapon):
     def __init__(self):
         super().__init__(munitions = 50, range = 100)
 
+    def fire_at(self, x: int, y: int, z: int):
+        if z != 0:
+            super().fire_at(x, y, z)# On fait appel à cette méthode pour dimunier les ammunitions
+            raise OutOfRangeError("la cible n'est pas admissible")
+        else:
+            print("la cible est atteignable")
